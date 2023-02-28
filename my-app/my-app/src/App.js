293e679d2a180c9996ac756e49b1d1885d5938b8
Your Name <antonio.zappatore.br@gmail.com>
2023-02-28 11:45:05 +0100

    Esercizio 3

---
+++ my-app/my-app/src/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function sum(a, b){
+    return <h2>The sum is {a + b}</h2>
+}
---
+++ my-app/my-app/src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+/* import "./index.css"; */
+import { sum } from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(sum(5,1));
