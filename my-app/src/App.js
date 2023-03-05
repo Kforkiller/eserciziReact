387fdf57c8aa7bf3263fc6d9b17c596a740ed0bc
Your Name <antonio.zappatore.br@gmail.com>
2023-03-05 18:59:35 +0100

    Esercizio less 65

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { Hello }  from "./Hello"
+import { Welcome } from "./Welcome";
 
-export function helloName(name){
-    return <h1>Hello, {name}</h1>
+export default class App extends React.Component{
+    render(){
+        return (
+            <div>
+                <Hello />
+                <Welcome name ={<strong>Antonio</strong>} age="65"/>
+            </div>
+        )
+    }
 }
