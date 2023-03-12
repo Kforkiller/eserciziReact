b5dd8b9c7464ceec279bc34174a4b72d168da1f2
Your Name <antonio.zappatore.br@gmail.com>
2023-03-12 20:59:28 +0100

    esercizio name interactive

---
+++ my-app/src/InteractiveWelcome.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Welcome } from "./Welcome"
+export class InteractiveWelcome extends React.Component{
+    state = {
+        name: ""
+    }
+    Changing = (event) => {
+        this.setState((state) => {
+            return {
+                name: event.target.value
+            }
+        })
+    }   
+    componentDidUpdate(){
+        console.log(this.state)
+    }
+    render(){
+        return (
+            <div>
+                <input type="text" name="name" value={this.state.name} onChange={this.Changing}/>
+                <Welcome name={this.state.name} age="21" />
+            </div>
+        )
+    }
+}
