f0326be4d75d3f89f25a14495a127fc0543686f3
Your Name <antonio.zappatore.br@gmail.com>
2023-03-12 19:23:18 +0100

    esercizio target

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,18 @@
 import React from "react";
+import ClickerCounter from "./ClickerCounter";
+import { Counter } from "./Counter";
+import { Hello }  from "./Hello"
+import { Welcome } from "./Welcome";
 
-export function helloName(name){
-    return <h1>Hello, {name}</h1>
+export default class App extends React.Component{
+    render(){
+        return (
+            <div>
+                <Hello />
+                <Welcome name="John" age="44" />
+                <Counter initialValue = {10} incrementInterval = {10000} incrementAmount = {50} />
+                <ClickerCounter/>
+            </div>
+        )
+    }
 }
