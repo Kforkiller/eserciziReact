086fa46adc4661d5ae091b9d1bdf8d827ec6b180
Your Name <antonio.zappatore.br@gmail.com>
2023-03-25 13:42:50 +0100

    esercizio reset

---
+++ my-app/src/TodoList.js
@@ -0,0 +1,43 @@
+import React from "react"
+
+export default class TodoList extends React.Component{
+    _input = React.createRef()
+    state = {
+        items: [
+            "Pulire casa",
+            "Fare benzina",
+            "Fare esercizi Develhope"
+        ]
+    }
+
+    handleAdd = () => {
+        this.setState((state) => {
+            const value = this._input.current.value
+            this._input.current.value = ""
+            return {
+                items: [...state.items, value]
+            }
+        })
+    }
+
+    handleReset = () => {
+        this.setState({
+            items: []
+        })
+    }
+
+    render(){
+        return (
+            <div>
+                <ul>
+                    {this.state.items.map((item, index) => {
+                        return <li key={index}>{item}</li>
+                    })}
+                </ul>
+                <input ref={this._input} type="text" />
+                <button onClick={this.handleAdd}>Add item</button>
+                <button onClick={this.handleReset}>Reset</button>
+            </div>
+        )
+    }
+}
