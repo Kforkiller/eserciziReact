9d6a5e4e35621f7693c49e5761f2c560a324a068
Kforkiller <119609625+Kforkiller@users.noreply.github.com>
2023-03-26 19:14:01 +0200

    Update Login.js

--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -1,6 +1,28 @@
 import React from "react";
 export class Login extends React.Component{
-    state = {
-        name: value
+     state = {
+        name: ""
+    }
+    ChangingEvent = (event) => {
+        const name = event.target.name
+        const value = event.target.value
+        const type = event.target.type
+        const checked = event.target.checked
+        this.setState({
+            [name]: type === `checkbox` ? checked : value
+    })
+}
+    componentDidUpdate(){
+        console.log(this.state)
+    }
+    render(){
+        return (
+            <div>
+                <input type="text" name="name" value={this.state.name} onChange={this.ChangingEvent}/>
+                <input type="password" name="password" value={this.state.password} onChange={this.ChangingEvent}/>
+                <input type="checkbox" name="checkbox" checked={this.state.checked} onChange={this.ChangingEvent}/>
+                <Welcome name={this.state.name} age="45" />
+            </div>
+        )
     }
 }
