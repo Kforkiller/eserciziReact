227674eca47869540f14d0e47aa97d7634f385f8
Your Name <antonio.zappatore.br@gmail.com>
2023-03-26 19:36:41 +0200

    Esercizio composition 1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,13 @@
-import React from "react";
+import { Container } from "./Container";
+import { Welcome } from "./Welcome";
 
-export function helloName(name){
-    return <h1>Hello, {name}</h1>
+export default function App() {
+    return (
+        <div>
+            <Container>
+                    <Welcome name="Giovanni" age={52} />
+                    <Welcome name="Antonio" age={17} />
+                </Container>
+        </div>
+    );
 }
