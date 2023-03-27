e1c8e8c0f7836d3bf1af095e5f23275d8f3bd182
Your Name <antonio.zappatore.br@gmail.com>
2023-03-28 01:40:39 +0200

    Era gia in h1 esercizio context 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,26 @@
 import React from "react";
+import { DisplayLanguage } from "./DisplayLanguage";
+import { LanguageContext } from "./LanguageContext";
 
-export function helloName(name){
-    return <h1>Hello, {name}</h1>
+export default class App extends React.Component{
+    state = {
+        language: "en"
+    }
+    render(){
+        return ( 
+        <div>
+        <select onChange={(evt) => {
+            this.setState({
+                language: evt.target.value
+            })
+        }}>
+            <option value="en">English</option>
+            <option value="it">Italiano</option>
+        </select>
+        <LanguageContext.Provider value={this.state.language}>
+            <DisplayLanguage />
+        </LanguageContext.Provider>
+    </div>
+)
+}
 }
