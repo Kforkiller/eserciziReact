546155adf2373e15406174f6e11fefbd44347493
Your Name <antonio.zappatore.br@gmail.com>
2023-03-29 01:05:33 +0200

    Esercizio hook 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { Form } from "./Form";
 
-export function helloName(name){
-    return <h1>Hello, {name}</h1>
+export default class App extends React.Component {
+
+  render() {
+    return (
+    <div>
+        <Form/>
+    </div>
+    )
+  }
 }
