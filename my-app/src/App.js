b6874900d8f710e4e491c63826ced13baa283bae
Your Name <antonio.zappatore.br@gmail.com>
2023-03-29 01:46:03 +0200

    Esercizio usecallback

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Form } from "./Form";
+import { Counter } from "./Counter";
 
 export default class App extends React.Component {
 
   render() {
     return (
     <div>
-        <Form/>
+        <Counter/>
     </div>
     )
   }
