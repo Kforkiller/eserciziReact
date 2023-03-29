56c676c8ded9f5080418e8f82c4037b8f4c3c9c0
Your Name <antonio.zappatore.br@gmail.com>
2023-03-29 02:43:48 +0200

    swr 2

---
+++ my-app/src/GitHubUser.js
@@ -0,0 +1,17 @@
+import { useGithubUser, useGitHubUser } from "./useGitHubUser"
+
+export function GitHubUser({ username }) {
+    const { user, error, isLoad } = useGithubUser(username)
+
+    return (
+        <div>
+            {isLoad && <h1>Loading...</h1>}
+            {error && <h1>{String(error)}</h1>}
+            {user && (
+                <div>
+                    <h1>{user.name}</h1>
+                </div>
+            )}
+        </div>
+    )
+}
