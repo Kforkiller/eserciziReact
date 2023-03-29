62700c4a4e42fc2f2f9e9ef8a9726b4ba68d0e5e
Your Name <antonio.zappatore.br@gmail.com>
2023-03-29 02:47:25 +0200

    route 3

---
+++ my-app/src/useGitHubUser.js
@@ -0,0 +1,14 @@
+import useSWR from "swr"
+
+const fetcher = url => fetch(url).then(result => result.json())
+
+export function useGithubUser(username){
+    const {info, error, mutate} = useSWR(username ? `https://api.github.com/users/${username}`: null, fetcher)
+
+    return {
+        user: info,
+        error,
+        isLoad: !info && !error,
+        mutate
+    }
+}
